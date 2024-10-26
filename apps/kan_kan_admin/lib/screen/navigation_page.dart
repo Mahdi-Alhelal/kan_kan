6156d67e950dee60de20d061a87fb73ef989f449
trt14 <tarooti14dev@gmail.com>
2024-10-26 16:00:44 +0300

    Supabase:Order (add-updateAllByDeal-updateOneOrder-fetchAll-fetchByUser) repository done

--- apps/kan_kan_admin/lib/screen/navigation_page.dart
+++ apps/kan_kan_admin/lib/screen/navigation_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kan_kan_admin/data/repositories/address_repository.dart';
 import 'package:kan_kan_admin/data/repositories/category_repository.dart';
 import 'package:kan_kan_admin/data/repositories/factory_repository.dart';
+import 'package:kan_kan_admin/data/repositories/order_repository.dart';
 import 'package:kan_kan_admin/data/repositories/product_repository.dart';
 import 'package:kan_kan_admin/screen/src/deals_screen.dart';
 import 'package:kan_kan_admin/screen/src/factory_screen.dart';
@@ -47,8 +48,11 @@ class NavigationPage extends StatelessWidget {
           children: [
             InkWell(
               onTap: () async {
-                final x = await ProductRepository.deleteProduct(
-                    id: "f28477ad-bbbe-442b-b29d-b0e0faff8cc2");
+                final x = await OrderRepository.addNewOrder(
+                    userID: "83efec21-2fc7-416e-9825-a86a8af3a63a",
+                    dealID: "89387763-f922-4c05-a372-2f9275238a9f",
+                    addressID: "686e90ff-0a01-4f95-ad32-98ddd29ef96c",
+                    amount: 300);
                 print(x.toString());
               },
               child: Container(
